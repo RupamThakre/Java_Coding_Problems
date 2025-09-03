b7ae0a408fe1bac2672e1e99d8356dc30b6a1058
RupamThakre <rupamthakre15@gmail.com>
2025-09-03 12:02:14 +0530

    Binary search recursive way

--- 27_BinarySearch/BinarySearchAlgo.java
+++ 27_BinarySearch/BinarySearchAlgo.java
@@ -11,6 +11,14 @@ class Main {
         System.out.println(iterativeBinarySearch(intArray, 35));
         System.out.println(iterativeBinarySearch(intArray, 20));
 
+        System.out.println(recursiveBinarySearch(intArray, -15));
+        System.out.println(recursiveBinarySearch(intArray, -22));
+        System.out.println(recursiveBinarySearch(intArray, 1));
+        System.out.println(recursiveBinarySearch(intArray, 7));
+        System.out.println(recursiveBinarySearch(intArray, 55));
+        System.out.println(recursiveBinarySearch(intArray, 35));
+        System.out.println(recursiveBinarySearch(intArray, 20));
+
     }
 
     public static int iterativeBinarySearch(int[] input, int value){
@@ -30,6 +38,33 @@ class Main {
             }
         }
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] input, int value){
+        return recursiveBinarySearch(input, 0, input.length, value);
+    }
+
+    public static int recursiveBinarySearch(int[] input, int start,
+                                            int end, int value) {
+        //It should have breaking condition
+
+        if(start >= end){
+            return -1;
+        }
+
+        int midPoint = (start + end)/2;
+        System.out.println("MidPoint : "+ midPoint);
+
+        if(input[midPoint] == value){
+            return midPoint;
+        }
+        else if(input[midPoint] < value){
+            return recursiveBinarySearch(input, midPoint + 1, end, value);
+        }
+        else {
+            return recursiveBinarySearch(input, start, midPoint, value);
+        }
+
     }
 
 }
